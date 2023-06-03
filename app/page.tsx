74f553be5bf42e6552822551300648ad8fd77708
Han <han-chen@ibuypower.com>
2023-06-04 02:14:26 +0800

    feat: rankbox

--- app/page.tsx
+++ app/page.tsx
@@ -85,18 +85,20 @@ export default function Home() {
           My Course
         </h1>
       </div>
-      <Timeline></Timeline>
-
-      {ranks.map((rank, i) => (
-        <div
-          key={`rank-${i}`}
-          className="m-4 w-1/3 border-separate border-spacing-2 border-4 border-[#c0f1a0] rounded-tr-3xl rounded-bl-3xl"
-        >
-          <p className="border border-slate-600 m-4 p-4 rounded-tr-3xl rounded-bl-3xl bg-[#b4e886]">
-            <span>{rank}</span>
-          </p>
+      <Timeline>
+        <div className="absolute top-0 left-[20%] md:left-[60%] w-full mt-8">
+          {ranks.map((rank, i) => (
+            <div
+              key={`rank-${i}`}
+              className="min-w-[250px] my-[160px] w-1/3 border-separate border-spacing-2 border-4 border-[#c0f1a0] rounded-tr-3xl rounded-bl-3xl"
+            >
+              <p className="border border-slate-600 m-4 p-4 rounded-tr-3xl rounded-bl-3xl bg-[#b4e886]">
+                <span>{rank}</span>
+              </p>
+            </div>
+          ))}
         </div>
-      ))}
+      </Timeline>
     </>
   );
 }
